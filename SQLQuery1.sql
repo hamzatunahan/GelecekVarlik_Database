0e2e0d8d032821188154fb715e246f8219809baf
Hamza Tunahan ARSLAN <hamzarslan34@gmail.com>
2022-04-26 16:00:54 +0300

    first commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,306 @@
+USE [master]
+GO
+/****** Object:  Database [Homework1]    Script Date: 19.04.2022 16:59:28 ******/
+CREATE DATABASE [Homework1]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'Homework1', FILENAME = N'D:\MsSql\Basic\MSSQL15.MSSQLSERVER\MSSQL\DATA\Homework1.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'Homework1_log', FILENAME = N'D:\MsSql\Basic\MSSQL15.MSSQLSERVER\MSSQL\DATA\Homework1_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+ALTER DATABASE [Homework1] SET COMPATIBILITY_LEVEL = 150
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [Homework1].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [Homework1] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [Homework1] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [Homework1] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [Homework1] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [Homework1] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [Homework1] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [Homework1] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [Homework1] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [Homework1] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [Homework1] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [Homework1] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [Homework1] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [Homework1] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [Homework1] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [Homework1] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [Homework1] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [Homework1] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [Homework1] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [Homework1] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [Homework1] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [Homework1] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [Homework1] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [Homework1] SET RECOVERY FULL 
+GO
+ALTER DATABASE [Homework1] SET  MULTI_USER 
+GO
+ALTER DATABASE [Homework1] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [Homework1] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [Homework1] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [Homework1] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [Homework1] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [Homework1] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'Homework1', N'ON'
+GO
+ALTER DATABASE [Homework1] SET QUERY_STORE = OFF
+GO
+USE [Homework1]
+GO
+/****** Object:  Table [dbo].[Account]    Script Date: 19.04.2022 16:59:29 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Account](
+	[Account_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Account_name] [varchar](40) NOT NULL,
+	[Account_description] [varchar](40) NOT NULL,
+	[Account_phone] [int] NOT NULL,
+	[Billing_address] [varchar](40) NOT NULL,
+ CONSTRAINT [PK_Account] PRIMARY KEY CLUSTERED 
+(
+	[Account_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AccountContactRole]    Script Date: 19.04.2022 16:59:29 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AccountContactRole](
+	[AccountContactRoleID] [int] IDENTITY(1,1) NOT NULL,
+	[Contact_ID] [int] NOT NULL,
+	[Account_ID] [int] NOT NULL,
+ CONSTRAINT [PK_AccountContactRole] PRIMARY KEY CLUSTERED 
+(
+	[AccountContactRoleID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Campaign]    Script Date: 19.04.2022 16:59:29 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Campaign](
+	[Campaign_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Campaign_name] [varchar](40) NOT NULL,
+	[Campaign_objectives] [varchar](40) NOT NULL,
+	[Campaign_sponsor] [varchar](40) NOT NULL,
+	[Campaign_start_date] [date] NOT NULL,
+	[Campaign_end_date] [date] NOT NULL,
+	[Campaign_other_details] [varchar](40) NULL,
+ CONSTRAINT [PK_Campaign] PRIMARY KEY CLUSTERED 
+(
+	[Campaign_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[CampaignMember]    Script Date: 19.04.2022 16:59:29 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[CampaignMember](
+	[Campaign_Member_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Campaign_ID] [int] NOT NULL,
+	[Lead_ID] [int] NOT NULL,
+	[Contact_ID] [int] NOT NULL,
+ CONSTRAINT [PK_Campaign_Member] PRIMARY KEY CLUSTERED 
+(
+	[Campaign_Member_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Case]    Script Date: 19.04.2022 16:59:29 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Case](
+	[Case_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Contact_ID] [int] NOT NULL,
+ CONSTRAINT [PK_Case] PRIMARY KEY CLUSTERED 
+(
+	[Case_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Contact]    Script Date: 19.04.2022 16:59:29 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Contact](
+	[Contact_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Account_ID] [int] NOT NULL,
+	[Contact_adress] [varchar](40) NOT NULL,
+	[Contact_contract_details] [varchar](40) NOT NULL,
+ CONSTRAINT [PK_Contact] PRIMARY KEY CLUSTERED 
+(
+	[Contact_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Contract]    Script Date: 19.04.2022 16:59:29 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Contract](
+	[ContractID] [int] IDENTITY(1,1) NOT NULL,
+	[Account_ID] [int] NOT NULL,
+	[Contract_status] [varchar](40) NOT NULL,
+	[Contract_approval] [varchar](40) NOT NULL,
+ CONSTRAINT [PK_Contract] PRIMARY KEY CLUSTERED 
+(
+	[ContractID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Lead]    Script Date: 19.04.2022 16:59:29 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Lead](
+	[Lead_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Lead_firstname] [varchar](40) NOT NULL,
+	[Lead_surname] [varchar](40) NOT NULL,
+	[Lead_other_details] [varchar](40) NOT NULL,
+ CONSTRAINT [PK_Lead] PRIMARY KEY CLUSTERED 
+(
+	[Lead_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Opportunity]    Script Date: 19.04.2022 16:59:29 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Opportunity](
+	[Opportunity_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Account_ID] [int] NOT NULL,
+	[Opportunity_description] [varchar](40) NOT NULL,
+	[Opportunity_details] [varchar](40) NOT NULL,
+	[Opportunity_stage] [varchar](40) NOT NULL,
+ CONSTRAINT [PK_Opportunity] PRIMARY KEY CLUSTERED 
+(
+	[Opportunity_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[OpportunityContactRole]    Script Date: 19.04.2022 16:59:29 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[OpportunityContactRole](
+	[OpportunityContactRole_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Contact_ID] [int] NOT NULL,
+	[Opportunity_ID] [int] NOT NULL,
+	[Date_time] [datetime] NOT NULL,
+	[Other_details] [varchar](40) NOT NULL,
+ CONSTRAINT [PK_OpportunityContactRole] PRIMARY KEY CLUSTERED 
+(
+	[OpportunityContactRole_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[AccountContactRole]  WITH CHECK ADD  CONSTRAINT [Reference_7] FOREIGN KEY([Contact_ID])
+REFERENCES [dbo].[Contact] ([Contact_ID])
+GO
+ALTER TABLE [dbo].[AccountContactRole] CHECK CONSTRAINT [Reference_7]
+GO
+ALTER TABLE [dbo].[AccountContactRole]  WITH CHECK ADD  CONSTRAINT [Reference_8] FOREIGN KEY([Account_ID])
+REFERENCES [dbo].[Account] ([Account_ID])
+GO
+ALTER TABLE [dbo].[AccountContactRole] CHECK CONSTRAINT [Reference_8]
+GO
+ALTER TABLE [dbo].[CampaignMember]  WITH CHECK ADD  CONSTRAINT [Reference_1] FOREIGN KEY([Campaign_ID])
+REFERENCES [dbo].[Campaign] ([Campaign_ID])
+GO
+ALTER TABLE [dbo].[CampaignMember] CHECK CONSTRAINT [Reference_1]
+GO
+ALTER TABLE [dbo].[CampaignMember]  WITH CHECK ADD  CONSTRAINT [Reference_2] FOREIGN KEY([Lead_ID])
+REFERENCES [dbo].[Lead] ([Lead_ID])
+GO
+ALTER TABLE [dbo].[CampaignMember] CHECK CONSTRAINT [Reference_2]
+GO
+ALTER TABLE [dbo].[CampaignMember]  WITH CHECK ADD  CONSTRAINT [Reference_3] FOREIGN KEY([Contact_ID])
+REFERENCES [dbo].[Contact] ([Contact_ID])
+GO
+ALTER TABLE [dbo].[CampaignMember] CHECK CONSTRAINT [Reference_3]
+GO
+ALTER TABLE [dbo].[Case]  WITH CHECK ADD  CONSTRAINT [Reference_6] FOREIGN KEY([Contact_ID])
+REFERENCES [dbo].[Contact] ([Contact_ID])
+GO
+ALTER TABLE [dbo].[Case] CHECK CONSTRAINT [Reference_6]
+GO
+ALTER TABLE [dbo].[Contact]  WITH CHECK ADD  CONSTRAINT [Reference_5] FOREIGN KEY([Account_ID])
+REFERENCES [dbo].[Account] ([Account_ID])
+GO
+ALTER TABLE [dbo].[Contact] CHECK CONSTRAINT [Reference_5]
+GO
+ALTER TABLE [dbo].[Contract]  WITH CHECK ADD  CONSTRAINT [Reference_11] FOREIGN KEY([Account_ID])
+REFERENCES [dbo].[Account] ([Account_ID])
+GO
+ALTER TABLE [dbo].[Contract] CHECK CONSTRAINT [Reference_11]
+GO
+ALTER TABLE [dbo].[Opportunity]  WITH CHECK ADD  CONSTRAINT [Reference_10] FOREIGN KEY([Account_ID])
+REFERENCES [dbo].[Account] ([Account_ID])
+GO
+ALTER TABLE [dbo].[Opportunity] CHECK CONSTRAINT [Reference_10]
+GO
+ALTER TABLE [dbo].[OpportunityContactRole]  WITH CHECK ADD  CONSTRAINT [Reference_4] FOREIGN KEY([Contact_ID])
+REFERENCES [dbo].[Contact] ([Contact_ID])
+GO
+ALTER TABLE [dbo].[OpportunityContactRole] CHECK CONSTRAINT [Reference_4]
+GO
+ALTER TABLE [dbo].[OpportunityContactRole]  WITH CHECK ADD  CONSTRAINT [Reference_9] FOREIGN KEY([Opportunity_ID])
+REFERENCES [dbo].[Opportunity] ([Opportunity_ID])
+GO
+ALTER TABLE [dbo].[OpportunityContactRole] CHECK CONSTRAINT [Reference_9]
+GO
+USE [master]
+GO
+ALTER DATABASE [Homework1] SET  READ_WRITE 
+GO
